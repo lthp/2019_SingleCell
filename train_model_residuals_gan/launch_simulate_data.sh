5577cb5de56b4ea5a5f4164985331ce3478fd4ae
hanna <hanna15@ru.is>
2019-12-07 20:13:43 +0100

    generalized vanilla gan and residual gan first attempt

---
+++ train_model_residuals_gan/launch_simulate_data.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+python simulate_data.py  \
+        --n_samples=10 \
+        --n_markers=20 \
+        --n_cells_min=1000 \
+        --n_cells_max=2000 \
+	--n_batches=2 \
+        --distribution="gamma" \
+        --path_save='/Users/joannaf/Desktop/courses/DeepLearning/DL2019/project/data/simulated/toy_data_gamma_small.parquet' \
+        --seed=234 \
+	--add_ri_patient=False
+
