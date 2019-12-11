1454322ebd68e5e2bc5193d2d1e6b21bc6970893
hanna <hanna15@ru.is>
2019-12-11 22:31:37 +0100

    create two seperate files for autoenc-gan with and without reconstruction loss for comparison, also add proper autoencoder generator with a bottleneck layer

---
+++ gan_autoencoder/autoencoder_gan_originalloss.py
@@ -0,0 +1,187 @@
+from __future__ import print_function, division
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout, LeakyReLU
+from tensorflow.keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+from visualisation_and_evaluation.helpers_vizualisation import plot_tsne, plot_metrics, plot_umap
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+
+class GAN():
+    def __init__(self, n_markers=30):
+        self.data_size = n_markers
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+
+        # The generator takes x1 as input and generates gen_x1 (fake x2)
+        x1 = Input(shape=(self.data_size,))
+        gen_x1 = self.generator(x1)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated data as input and determines validity
+        validity = self.discriminator(gen_x1)
+
+        # The combined model  (stacked generator and discriminator)
+        # Trains the generator to fool the discriminator
+        self.combined = Model(inputs=x1, outputs=[gen_x1, validity])
+        losses = ['mean_absolute_error', 'binary_crossentropy']
+        loss_weights = [1, 0.1]
+        self.combined.compile(loss=losses, optimizer=optimizer, loss_weights=loss_weights)
+
+
+    def build_generator(self):
+        model = Sequential()
+        model.add(Dense(30, input_dim=self.data_size))
+        model.add(LeakyReLU(alpha=0.2))
+
+        model.add(Dense(40))
+        model.add(LeakyReLU(alpha=0.2))
+
+        model.add(Dense(50))
+        model.add(LeakyReLU(alpha=0.2))
+
+        model.add(Dense(40))
+        model.add(LeakyReLU(alpha=0.2))
+
+        model.add(Dense(30))
+        model.add(LeakyReLU(alpha=0.2))
+
+        model.add(Dense(self.data_size, activation='tanh'))
+        # model.add(Reshape(self.img_shape))
+        model.summary()
+
+        x1 = Input(shape=(self.data_size,))
+        x1_gen = model(x1)
+
+        return Model(x1, x1_gen)
+
+
+    def build_discriminator(self):
+        model = Sequential()
+        model.add(Dense(512, input_shape=(self.data_size,)))
+        model.add(LeakyReLU(alpha=0.2))
+
+        model.add(Dense(256))
+        model.add(LeakyReLU(alpha=0.2))
+
+        model.add(Dense(128))
+        model.add(LeakyReLU(alpha=0.2))
+
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+
+        x2 = Input(shape=self.data_size)
+        validity = model(x2)
+        return Model(x2, validity)
+
+
+    def train(self, x1_df, x2_df, epochs, batch_size=128, sample_interval=50):
+        fname = datetime.now().strftime("%d-%m-%Y_%H.%M.%S")
+        os.makedirs(os.path.join('figure_originalloss', fname))
+
+        plot_model = {"epoch": [], "d_loss": [], "g_loss": []}
+
+        x1_train = x1_df.values
+        x2_train = x2_df.values
+
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+        d_loss = [0, 0]
+
+        steps_per_epoch = len(x1_train) // batch_size
+        for epoch in range(epochs):
+
+            for step in range(steps_per_epoch):
+                # ---------------------
+                #  Train Discriminator
+                # ---------------------
+
+                # Select a random batch of x1 and x2
+                idx = np.random.randint(0, x1_train.shape[0], batch_size)
+                x1 = x1_train[idx]
+                x2 = x2_train[idx]
+
+                # Generate a batch of new images
+                gen_x1 = self.generator.predict(x1)
+
+                # Train the discriminator
+                if d_loss[1] > 0.8:
+                    d_loss_real = self.discriminator.test_on_batch(x2, valid)
+                    d_loss_fake = self.discriminator.test_on_batch(gen_x1, fake)
+                    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+                    pass
+                else:
+                    d_loss_real = self.discriminator.train_on_batch(x2, valid)
+                    d_loss_fake = self.discriminator.train_on_batch(gen_x1, fake)
+                    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+                # ---------------------
+                #  Train Generator
+                # ---------------------
+
+                # Train the generator (to have the discriminator label samples as valid)
+                g_loss = self.combined.train_on_batch(x1, [x1, valid])
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f, mae: %.2f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss[1], g_loss[0]))
+
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                # self.sample_x2(epoch, x1)
+                self.plot_progress(epoch, x1_df, x2_df, plot_model, fname)
+
+    def transform_batch(self, x):
+        gx = self.generator.predict(x)
+        gx_df = pd.DataFrame(data=gx, columns=x.columns, index=x.index + '_transformed')
+        return gx_df
+
+    def plot_progress(self, epoch, x1, x2, metrics, fname):
+        plot_metrics(metrics, os.path.join('figure_originalloss', fname, 'metrics'))
+        if epoch == 0:
+            plot_tsne(pd.concat([x1, x2]), do_pca=True, n_plots=2, iter_=500, pca_components=20,
+                      save_as=os.path.join(fname, 'aegan_tsne_x1-x2_epoch'+str(epoch)), folder_name='figure_originalloss')
+            plot_umap(pd.concat([x1, x2]), save_as=os.path.join(fname, 'aegan_umap_x1-x2_epoch'+str(epoch)),
+                      folder_name='figure_originalloss')
+
+        gx1 = self.generator.predict(x1)
+        gx1 = pd.DataFrame(data=gx1, columns=x1.columns, index=x1.index + '_transformed')
+        plot_tsne(pd.concat([x1, gx1]), do_pca=True, n_plots=2, iter_=500, pca_components=20,
+                  save_as=os.path.join(fname, 'aegan_tsne_x1-gx1_epoch'+str(epoch)), folder_name='figure_originalloss')
+        plot_tsne(pd.concat([gx1, x2]), do_pca=True, n_plots=2, iter_=500, pca_components=20,
+                  save_as=os.path.join(fname, 'aegan_tsne_gx1-x2_epoch'+str(epoch)), folder_name='figure_originalloss')
+        plot_umap(pd.concat([x1, gx1]), save_as=os.path.join(fname, 'aegan_umap_gx1-x1_epoch'+str(epoch)),
+                  folder_name='figure_originalloss')
+        plot_umap(pd.concat([x2, gx1]), save_as=os.path.join(fname, 'aegan_umap_gx1-x2_epoch'+str(epoch)),
+                  folder_name='figure_originalloss')
+
+
+if __name__ == '__main__':
+    import os
+    from loading_and_preprocessing.data_loader import load_data_basic, load_data_cytof
+    # path = r'C:\Users\heida\Documents\ETH\Deep Learning\2019_DL_Class_old\code_ADAE_\chevrier_data_pooled_panels.parquet'
+    path = r'C:\Users\Public\PycharmProjects\deep\Legacy_2019_DL_Class\data\chevrier_data_pooled_panels.parquet'
+    x1_train, x1_test, x2_train, x2_test = load_data_cytof(path, patient_id='rcc7', n=10000)
+
+    #path = os.getcwd()
+    #path = path + '/toy_data_gamma_small.parquet'  # '/toy_data_gamma_large.parquet'
+    #x1_train, x1_test, x2_train, x2_test = load_data_basic(path, patient='sample1', batch_names=['batch1', 'batch2'],
+    #                                                       seed=42, n_cells_to_select=0)
+    gan = GAN(x1_train.shape[1])
+    gan.train(x1_train, x2_train, epochs=3000, batch_size=64, sample_interval=50)
