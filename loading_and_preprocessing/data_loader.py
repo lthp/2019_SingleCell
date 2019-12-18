e31abd90977c45e73afaf774a8846f7685944a05
Joanna Ficek <joanna.ficek@inf.ethz.ch>
2019-12-18 15:22:16 +0100

    data loader updated

--- loading_and_preprocessing/data_loader.py
+++ loading_and_preprocessing/data_loader.py
@@ -10,39 +10,56 @@ def normalize(x):
     return pd.DataFrame(data=data, columns=x.columns, index=x.index)
 
 
-def load_data_basic(path, patient='sample1', batch_names=['batch1', 'batch2'], seed=42,
-                    n_cells_to_select=500):
+def load_data_basic(path, sample='sample1', batch_names=['batch1', 'batch2'], 
+                    panel=None, seed=42, n_cells_to_select=0, test_size=0.2):
     """
     Function to load data and split into 2 inputs with train and test sets
     inputs:
         path: path to the data file
-        patient: name of the patient to consider
+        sample: name of the sample to consider
         batch_names: a list of batch names to split the data
-        n_cells_to_select: number of cells to select for quicker runs, if 0 then all cells are selected
+        n_cells_to_select: number of cells to select for quicker runs, if 0 then all cells are selected (min of the 2 batches)
+        test_size: proportion of the test set
     outputs:
         x1_train, x1_test: train and test sets form the first batch
         x2_train, x2_test: train and test sets form the second batch
     """
     df = pd.read_parquet(path, engine='pyarrow')
+    if(panel is not None):
+        df = df.loc[df['metadata_panel'].str.startswith(panel),:]
+        # update batches names that are present in the panel
+        panel_batch_names = list(set(df.loc[:,'metadata_batch']))
+        if(len([x for x in batch_names if x not in panel_batch_names])):
+            batch_names = panel_batch_names
+    # remove columns with ann (bcs of merging the panels)
+    df = df.dropna(axis=1)
+    if('metadata_batch' in df.columns and 'metadata_sample' in df.columns):
+        x1 = df.loc[(df['metadata_batch']==batch_names[0]) & (df['metadata_sample']==sample),:].copy()
+        x2 = df.loc[(df['metadata_batch']==batch_names[1]) & (df['metadata_sample']==sample),:].copy()
+    else:
+        idx = df.index.get_values()
+        x1_idx = [x for x in idx if sample in x and batch_names[0] in x and sample+'0' not in x]
+        x1_idx = [i for (i,t) in enumerate(idx) if t in x1_idx]
+        x1 = df.loc[x1_idx, :].copy()
+        x2_idx = [x for x in idx if sample in x and batch_names[1] in x and sample+'0' not in x]
+        x2_idx = [i for (i,t) in enumerate(idx) if t in x2_idx]
+        x2 = df.loc[x2_idx, :].copy()
+    # remove metadata columns
     selected_cols = [col for col in df.columns if "metadata" not in col]
-    df = df.loc[:, selected_cols]
-    idx = df.index.get_values()
-    x1_idx = [x for x in idx if patient in x and batch_names[0] in x and patient+'0' not in x][0]
-    x1 = df.loc[x1_idx, :].copy()
-    x2_idx = [x for x in idx if patient in x and batch_names[1] in x and patient+'0' not in x][0]
-    x2 = df.loc[x2_idx, :].copy()
+    x1 = x1.loc[:, selected_cols]
+    x2 = x2.loc[:, selected_cols]
     if n_cells_to_select > 0:
-        cells_to_select = np.random.uniform(0, x1.shape[0], n_cells_to_select)
-        x1 = x1.iloc[cells_to_select, :]
-        cells_to_select = np.random.uniform(0, x2.shape[0], n_cells_to_select)
-        x2 = x2.iloc[cells_to_select, :]
+        n_cells_to_select = np.min([n_cells_to_select,x1.shape[0], x2.shape[0]])
+    else:
+        n_cells_to_select = np.min([x1.shape[0], x2.shape[0]])
+    cells_to_select = np.random.uniform(0, x1.shape[0], n_cells_to_select)
+    x1 = x1.iloc[cells_to_select, :]
+    cells_to_select = np.random.uniform(0, x2.shape[0], n_cells_to_select)
+    x2 = x2.iloc[cells_to_select, :]
     x1 = normalize(x1)
     x2 = normalize(x2)
-    n = np.min([len(x1), len(x2)])
-    x1 = x1[:n]
-    x2 = x2[:n]
-    x1_train, x1_test = train_test_split(x1, test_size=0.2, random_state=42)
-    x2_train, x2_test = train_test_split(x2, test_size=0.2, random_state=42)
+    x1_train, x1_test = train_test_split(x1, test_size=test_size, random_state=seed)
+    x2_train, x2_test = train_test_split(x2, test_size=test_size, random_state=seed)
     return x1_train, x1_test, x2_train, x2_test
 
 
