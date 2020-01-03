920f8c01a86f18f8bf2f8fef19f17e96ba5c4a0c
hthrainsson <hthrainsson@student.ethz.ch>
2020-01-03 01:46:00 +0100

    Fixed big in resgan runscript

--- residual_gan/run_residuals.sh
+++ residual_gan/run_residuals.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 source activate deeplearning
-data_loc = '/cluster/home/hthrainsson/chevrier_data_pooled_full_panels.parquet'
-
-lambda = 0.9
-sample_no = 5
+data_loc='/cluster/home/hthrainsson/chevrier_data_pooled_full_panels.parquet'
+lambda=0.9
+sample_no=5
+echo "python residual_gan/residual_gan_autoencoder.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
+echo "python residual_gan/residual_gan_autoencoder_without_residuals.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
+sample_no=65
 echo "python residual_gan/residual_gan_autoencoder.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
 echo "python residual_gan/residual_gan_autoencoder_without_residuals.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
 
-sample_no = 65
+sample_no=75
 echo "python residual_gan/residual_gan_autoencoder.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
 echo "python residual_gan/residual_gan_autoencoder_without_residuals.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
 
-sample_no = 75
+lambda=0.8
+sample_no=5
 echo "python residual_gan/residual_gan_autoencoder.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
 echo "python residual_gan/residual_gan_autoencoder_without_residuals.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
 
-lambda = 0.8
-sample_no = 5
+sample_no=65
 echo "python residual_gan/residual_gan_autoencoder.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
 echo "python residual_gan/residual_gan_autoencoder_without_residuals.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
 
-sample_no = 65
-echo "python residual_gan/residual_gan_autoencoder.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
-echo "python residual_gan/residual_gan_autoencoder_without_residuals.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
-
-sample_no = 75
+sample_no=75
 echo "python residual_gan/residual_gan_autoencoder.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
 echo "python residual_gan/residual_gan_autoencoder_without_residuals.py ${sample_no} ${data_loc} ${lambda}"|  bsub -n 1 -W 24:00 -R "rusage[mem=5000]"
